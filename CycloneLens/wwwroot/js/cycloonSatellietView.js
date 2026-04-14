8b998b05478e3fd58fe13aa6faada8414f32251c
ea433 <i576652@fontysict.nl>
2026-04-14 23:50:38 +0200

    implementatie satellietafbeelding NOAA GOES-16 Geocolor True Color daytime, multispectral IR night

--- CycloneLens/wwwroot/js/cycloonSatellietView.js
+++ CycloneLens/wwwroot/js/cycloonSatellietView.js
@@ -1,5 +1,8 @@
-﻿const imageUrl = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/GEOCOLOR/1000x1000.jpg";
+﻿function updateGOES() {
+    const img = document.getElementById("goesImage");
+    const timestamp = new Date().getTime();
+    img.src = `https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/tatl/GEOCOLOR/latest.jpg?t=${timestamp}`;
+}
 
-document.getElementById("satImage").src = imageUrl;
-document.getElementById("time").textContent = "2026-04-07 14:30 UTC";
-document.getElementById("location").textContent = "23.5°N, 65.2°W";
+updateGOES();
+setInterval(updateGOES, 600000);
