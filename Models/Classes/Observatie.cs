0e345fbd2fcf487f494f45a83898b98d90d4c36c
ea433 <i576652@fontysict.nl>
2026-04-17 00:15:18 +0200

    class Observatie aangemaakt

---
+++ Models/Classes/Observatie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Types;
+using System.Text;
+
+namespace Models.Classes
+{
+    public class Observatie
+    {
+        public int Id { get; }
+        public string Omschrijving { get; private set; }
+        public string AfbeeldingPad { get; private set; }
+        public SqlGeography Coordinaten { get; private set; }
+        public DateTime Tijdstip { get; private set; }
+
+        public Observatie(int id, string omschrijving, string afbeeldingPad,
+            SqlGeography coordinaten, DateTime tijdstip)
+        {
+            this.Id = Id;
+            this.Omschrijving = omschrijving ?? throw new Exception(nameof(omschrijving));
+            this.AfbeeldingPad = afbeeldingPad ?? throw new Exception(nameof(afbeeldingPad));
+            this.Coordinaten = coordinaten ?? throw new Exception(nameof(coordinaten));
+            this.Tijdstip = tijdstip;
+        }
+    }
+}
