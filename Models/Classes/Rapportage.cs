76d4375966964d696eaafd03271aea9a80d919eb
ea433 <i576652@fontysict.nl>
2026-05-07 15:49:25 +0200

    class Rapportage toegevoegd

---
+++ Models/Classes/Rapportage.cs
@@ -0,0 +1,23 @@
+﻿using CycloneLens.Models;
+using Models.Enums;
+
+namespace Models.Classes
+{
+    public class Rapportage
+    {
+        public int Id { get; private set; }
+        public RapportageStatusType Status { get; private set; }
+        public DateTime Tijdstip { get; private set; }
+        public Gebruiker Gebruiker { get; private set; }
+        public Observatie Observatie { get; private set; }
+
+        public Rapportage(int Id, RapportageStatusType Status, DateTime Tijdstip, Gebruiker Gebruiker, Observatie Observatie)
+        {
+            this.Id = Id;
+            this.Status = Status;
+            this.Tijdstip = Tijdstip;
+            this.Gebruiker = Gebruiker;
+            this.Observatie = Observatie;
+        }
+    }
+}
