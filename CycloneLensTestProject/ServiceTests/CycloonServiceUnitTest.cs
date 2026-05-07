5a58be600a0e51686bccf4b64b658148ae06bf25
ea433 <i576652@fontysict.nl>
2026-05-07 15:47:11 +0200

    meer klassen uit CycloneService getest in CycloonServiceUnitTest

--- CycloneLensTestProject/ServiceTests/CycloonServiceUnitTest.cs
+++ CycloneLensTestProject/ServiceTests/CycloonServiceUnitTest.cs
@@ -1,6 +1,7 @@
-﻿using Interface_Layer.DTOs;
+﻿using CycloneLensTestProject.FakeRepositories;
+using Interface_Layer.DTOs;
 using Logic.Enums;
-using CycloneLensTestProject.FakeRepositories;
+using Microsoft.SqlServer.Types;
 
 // onafgemaakt 
 
@@ -8,6 +9,107 @@ namespace CycloneLensTestProject.ServiceTests
 {
     public class CycloonServiceUnitTest
     {
+        [Fact]
+        public void GetActiveCyclonenNATL_ReturnsOnlyActiveNorthAtlanticCyclones()
+        {
+            // Arrange
+            var cycloonRepo = new FakeCycloonRepository
+            {
+                Cyclonen = new List<CycloonDTO>
+        {
+            new CycloonDTO
+            {
+                Id = 1,
+                Naam = "Maria",
+                Status = "Actief",
+                Bassin = "Noord-Atlantisch"
+            },
+            new CycloonDTO
+            {
+                Id = 2,
+                Naam = "Jose",
+                Status = "Inactief",
+                Bassin = "Noord-Atlantisch"
+            },
+            new CycloonDTO
+            {
+                Id = 3,
+                Naam = "Lee",
+                Status = "Actief",
+                Bassin = "Stille-Oceaan"
+            }
+        }
+            };
+
+            var metadataRepo = new FakeMetadataRepository
+            {
+                Metadata = new List<MetadataDTO>()
+            };
+
+            var loggingRepo = new FakeLoggingRepository();
+
+            var service = new CycloonService(cycloonRepo, metadataRepo, loggingRepo);
+
+            // Act
+            var result = service.GetActiveCyclonenNATL();
+
+            // Assert
+            Assert.Single(result);
+            Assert.Equal("Maria", result[0].Naam);
+        }
+
+        [Fact]
+        public void GetActiveCyclonenNATL_UsesLatestMetadata()
+        {
+            // Arrange
+            var cycloonRepo = new FakeCycloonRepository
+            {
+                Cyclonen = new List<CycloonDTO>
+        {
+            new CycloonDTO
+            {
+                Id = 1,
+                Naam = "Maria",
+                Status = "Actief",
+                Bassin = "Noord-Atlantisch"
+            }
+        }
+            };
+
+            var metadataRepo = new FakeMetadataRepository
+            {
+                Metadata = new List<MetadataDTO>
+        {
+            new MetadataDTO
+            {
+                CycloonId = 1,
+                Categorie = (int)CategorieType.Categorie_1,
+                Tijdstip = new DateTime(2024,1,1),
+                Coordinaten = SqlGeography.Point(0, 0, 4326)
+            },
+
+            new MetadataDTO
+            {
+                CycloonId = 1,
+                Categorie = (int)CategorieType.Categorie_5,
+                Tijdstip = new DateTime(2024,2,1),
+                Coordinaten = SqlGeography.Point(0, 0, 4326)
+            }
+        }
+            };
+
+            var service = new CycloonService(
+                cycloonRepo,
+                metadataRepo,
+                new FakeLoggingRepository());
+
+            // Act
+            var result = service.GetActiveCyclonenNATL();
+
+            // Assert
+            Assert.Equal(CategorieType.Categorie_5, result[0].Categorie);
+        }
+        
         [Fact]
         public void GetById_ReturnsMappedCycloon_WhenDtoExists()
         {
