13433c5c28321651048b408fe8cb2e2d7111c9df
ea433 <i576652@fontysict.nl>
2026-05-11 10:21:59 +0200

    model ForecastPoint toegevoegd

---
+++ Models/Classes/ForecastPoint.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SqlServer.Types;
+
+namespace Models.Classes
+{
+    public class ForecastPoint
+    {
+        public SqlGeography Coordinaten { get; set; } 
+
+        public int VoorspellingUur { get; set; }
+
+        public int Windsnelheid { get; set; }
+
+        public ForecastPoint(SqlGeography coordinaten, int voorspellingUur, int windsnelheid)
+        {
+            this.Coordinaten = coordinaten;
+            this.VoorspellingUur = voorspellingUur;
+            this.Windsnelheid = windsnelheid;
+        }
+    }
+}
