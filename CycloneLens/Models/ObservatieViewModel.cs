72097d13944ec939f97d49eb3c5fb2c7460d9023
ea433 <i576652@fontysict.nl>
2026-05-17 19:52:56 +0200

    ObservatieViewModel aangevuld

--- CycloneLens/Models/ObservatieViewModel.cs
+++ CycloneLens/Models/ObservatieViewModel.cs
@@ -2,9 +2,12 @@
 {
     public class ObservatieViewModel
     {
+        public int Id { get; set; } 
         public int CycloonId { get; set; }
         public string? Omschrijving { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public string? Inzender { get; set; }  
+        public DateTime Datum { get; set; }  
     }
 }
