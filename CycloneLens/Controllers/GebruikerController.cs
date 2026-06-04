99482a6d9870881984b7e463051b8ec15e3297d9
ea433 <i576652@fontysict.nl>
2026-06-04 22:27:23 +0200

    GebruikerController & view aangepast

--- CycloneLens/Controllers/GebruikerController.cs
+++ CycloneLens/Controllers/GebruikerController.cs
@@ -1,6 +1,64 @@
-﻿namespace Presentation.Controllers
+﻿using Business_Logic_Layer.Services;
+using Data_Access_Layer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Models.Classes;
+using Presentation.Models;
+
+namespace Presentation.Controllers
 {
-    public class GebruikerController
+    public class GebruikerController : Controller
     {
+        private readonly GebruikerService _service;
+
+        public GebruikerController(IConfiguration config)
+        {
+            string? connectionString = config.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrEmpty(connectionString))
+                throw new Exception("Connection string not found");
+
+            GebruikerRepository repository = new(connectionString);
+            _service = new GebruikerService(repository);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(GebruikerViewModel model)
+        {
+            try
+            {
+                _service.RegistreerGebruiker(model.Gebruikersnaam, model.Wachtwoord);
+                return RedirectToAction("Login");
+            }
+            catch (ArgumentException ex)
+            {
+                ViewBag.Fout = ex.Message;
+                return View(model);
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(GebruikerViewModel model)
+        {
+            Gebruiker? gebruiker = _service.Login(model.Gebruikersnaam, model.Wachtwoord);
+
+            if (gebruiker == null)
+            {
+                ViewBag.Fout = "Onjuiste gebruikersnaam of wachtwoord.";
+                return View(model);
+            }
+
+            HttpContext.Session.SetInt32("UserId", gebruiker.Id);
+            HttpContext.Session.SetInt32("UserType", (int)gebruiker.UserType);
+            return RedirectToAction("Index", "Cycloon");
+        }
     }
 }
