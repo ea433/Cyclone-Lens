261ffd34ac564c0b1fbfd0d617b2f254374c762d
ea433 <i576652@fontysict.nl>
2026-06-04 21:43:26 +0200

    GebruikerRepository Registreer

---
+++ Data Access Layer/Repositories/GebruikerRepository.cs
@@ -0,0 +1,81 @@
+﻿using Interface_Layer.DTOs;
+using Interface_Layer.InterfaceRepositories;
+using Microsoft.Data.SqlClient;
+
+namespace Data_Access_Layer.Repositories
+{
+    public class GebruikerRepository : IGebruikerRepository
+    {
+        private readonly string _connectionString;
+
+        public GebruikerRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void RegistreerGebruiker(GebruikerDTO gebruiker)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"INSERT INTO Gebruiker (gebruikersnaam, wachtwoord_hash, UserType) 
+                                     VALUES (@gebruikersnaam, @wachtwoordHash, @userType)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@gebruikersnaam", gebruiker.Gebruikersnaam);
+                        cmd.Parameters.AddWithValue("@wachtwoordHash", gebruiker.WachtwoordHash);
+                        cmd.Parameters.AddWithValue("@userType", gebruiker.UserType);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (SqlException databaseException)
+            {
+                throw new Exception("Databasefout bij registreren van gebruiker.", databaseException);
+            }
+        }
+
+        public GebruikerDTO? GetByGebruikersnaam(string gebruikersnaam)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT * FROM Gebruiker WHERE gebruikersnaam = @gebruikersnaam";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@gebruikersnaam", gebruikersnaam);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                return new GebruikerDTO
+                                {
+                                    Id = (int)reader["id"],
+                                    Gebruikersnaam = reader["gebruikersnaam"]?.ToString() ?? "",
+                                    WachtwoordHash = reader["wachtwoord_hash"]?.ToString() ?? "",
+                                    UserType = (int)reader["UserType"]
+                                };
+                            }
+                        }
+                    }
+                }
+
+                return null;
+            }
+            catch (SqlException databaseException)
+            {
+                throw new Exception("Databasefout bij ophalen van gebruiker.", databaseException);
+            }
+        }
+    }
+}
