205fb1ce411ceb6c822fbe324b81f7748e9bab8d
ea433 <i576652@fontysict.nl>
2026-06-06 18:41:01 +0200

    FakeGebruikerRepository toegevoegd

---
+++ CycloneLensTestProject/FakeRepositories/FakeGebruikerRepository.cs
@@ -0,0 +1,20 @@
+﻿using Interface_Layer.DTOs;
+using Interface_Layer.InterfaceRepositories;
+
+namespace CycloneLensTestProject.FakeRepositories
+{
+    public class FakeGebruikerRepository : IGebruikerRepository
+    {
+        public List<GebruikerDTO> _gebruikers = new();
+
+        public void RegistreerGebruiker(GebruikerDTO gebruiker)
+        {
+            _gebruikers.Add(gebruiker);
+        }
+
+        public GebruikerDTO? GetByGebruikersnaam(string gebruikersnaam)
+        {
+            return _gebruikers.FirstOrDefault(g => g.Gebruikersnaam == gebruikersnaam);
+        }
+    }
+}
